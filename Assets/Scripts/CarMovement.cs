bd2d44717f388a9f80e44e761e481bef90190862
Hector Borges <hectorborges94@gmail.com>
2019-01-25 20:36:23 -0600

    Added car movement

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public float speed;
+    public float clamp;
+
+    private void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        movement *= speed * Time.deltaTime;
+        transform.position += movement;
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -clamp, clamp), 0, 0);
+    }
+}
